92ec11d6f9658404cbb542902e70c7274fd43b51
Bit576 <silverstream323@gmail.com>
2025-04-02 20:46:04 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,214 @@
+document.getElementById("photoshopButton").addEventListener("click", function() {
+    const Color1 = `rgb(49, 168, 255)`;
+    const Color2 = `rgb(0, 24, 51)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Photoshop 2024</span><br><br>Adobe Photoshop — это мощное программное обеспечение для редактирования изображений, графического дизайна и цифровой живописи. Оно предлагает передовые инструменты для ретуширования, создания сложных композиций и работы с текстом и графикой. Photoshop широко используется в фотографии, дизайне, анимации и веб-разработке.";
+    var icon = "photos/photoshop.png";
+    var img = "photos/ps1.png"; 
+    var href="https://mega.nz/file/ifwXSQRB#CKJC4-NYbVVgnB2a_ashBYO0wPE4wsW75i3bSFROktc";
+    showFullScreenSquare(icon, description, Color1, Color2, img,href);
+});
+
+document.getElementById("lightroomButton").addEventListener("click", function() {
+    const Color1 = `rgb(49, 168, 255)`;
+    const Color2 = `rgb(0, 24, 51)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Lightroom CC</span><br><br>Adobe Lightroom CC — это профессиональное ПО для обработки, улучшения и каталогизации цифровых фотографий. Оно позволяет работать с изображениями в формате RAW, имеет мощные инструменты для цветокоррекции и создания фотогалерей, что делает его идеальным для профессиональных фотографов.";
+    var icon = "photos/lt.png";
+    var img = "photos/lt1.jpg"; 
+    var href="https://mega.nz/file/iWh1HYqK#_LZAcJLLw9SIQWDgORkxVHzWfsRFGR6cjJt_dR5gDtQ";
+    showFullScreenSquare(icon, description, Color1, Color2, img,href);
+});
+
+document.getElementById("lightroomClassicButton").addEventListener("click", function() {
+    const Color1 = `rgb(49, 168, 255)`;
+    const Color2 = `rgb(0, 24, 51)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Lightroom Classic</span><br><br>Adobe Lightroom Classic — это традиционная версия Lightroom, ориентированная на профессиональных фотографов. Она поддерживает работу с большими библиотеками изображений и предлагает углубленные инструменты для обработки и печати, включая усовершенствованную систему каталогизации и управление метаданными.";
+    var icon = "photos/ltc.png";
+    var img = "photos/ltc1.png"; 
+    var href="https://mega.nz/file/SXYiTRTL#Baj9PDd6JoYazd06cUNyEYnQE0YIV5YgYPv5Uve30jU";
+    showFullScreenSquare(icon, description, Color1, Color2, img,href);
+});
+
+document.getElementById("inCopyButton").addEventListener("click", function() {
+    const Color1 = `rgb(255, 51, 102)`;
+    const Color2 = `rgb(73, 2, 31)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe InCopy 2024</span><br><br>Adobe InCopy — это текстовый редактор, разработанный для профессионалов в области издательского дела. InCopy идеально интегрируется с Adobe InDesign и позволяет редакторам и писателям работать над текстами и стилями, не вмешиваясь в дизайн страницы.";
+    var icon = "photos/ic.png";
+    var img = "photos/ic1.png"; 
+    var href="https://mega.nz/file/zLB0gIaK#DO6n9rLwvw6IlhY4r2Fku8pnb-yvfwTKtyEcHHeomnQ";
+    showFullScreenSquare(icon, description, Color1, Color2, img,href);
+});
+
+document.getElementById("indesignButton").addEventListener("click", function() {
+    const Color1 = `rgb(255, 51, 102)`;
+    const Color2 = `rgb(73, 2, 31)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe InDesign 2024</span><br><br>Adobe InDesign — это ведущая программа для профессиональной верстки и дизайна печатных и цифровых изданий. Она позволяет создавать сложные макеты для журналов, книг, брошюр и других публикаций, а также поддерживает интеграцию с другими продуктами Adobe для более удобного рабочего процесса.";
+    var icon = "photos/id.png";
+    var img = "photos/id1.png";
+    var href="https://mega.nz/file/jGQ20YyB#vHsn1Nx74m3gVbPUxEusoeQaZe0B-9qUkoF0QdwLAho"; 
+    showFullScreenSquare(icon, description, Color1, Color2, img,href);
+});
+
+document.getElementById("dreamweaverButton").addEventListener("click", function() {
+    const Color1 = `rgb(255, 97, 246)`;
+    const Color2 = `rgb(71, 1, 55)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Dreamweaver 2024</span><br><br>Adobe Dreamweaver — это интегрированная среда разработки для веб-сайтов. Поддерживает работу с HTML, CSS, JavaScript и PHP, а также предоставляет мощные инструменты для создания адаптивных веб-страниц и визуальной разработки с использованием различных фреймворков.";
+    var icon = "photos/dw.png";
+    var img = "photos/dw1.png"; 
+    var href="https://mega.nz/file/7D4E0R7B#amaz44XvUefJn9bXQmF-ma3GjGYwedi_eiT8fsnFJAE";
+    showFullScreenSquare(icon ,description, Color1, Color2, img,href);
+});
+
+document.getElementById("illustratorButton").addEventListener("click", function() {
+    const Color1 = `rgb(255, 154, 0)`;
+    const Color2 = `rgb(51, 0, 0)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Illustrator 2024</span><br><br>Adobe Illustrator — это векторный редактор, используемый для создания иллюстраций, логотипов, иконок и другой графики. Он идеален для создания четких и масштабируемых изображений, а также поддерживает сложные дизайнерские элементы, такие как типографику и рисунки.";
+    var icon = "photos/illustrator.png";
+    var img = "photos/il1.jpg";
+    var href="https://mega.nz/file/ef50wQzD#CV-h2DIfpTnRIZ2fr_WljjuIuyZYvcU7OEt3FwQtMJ0";
+    showFullScreenSquare(icon ,description, Color1, Color2, img,href);
+});
+
+document.getElementById("afterEffectsButton").addEventListener("click", function() {
+    const Color1 = `rgb(153, 153, 255)`;
+    const Color2 = `rgb(0, 0, 91)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe After Effects 2024</span><br><br>Adobe After Effects — это мощная программа для создания анимаций, визуальных эффектов и композитинга. Широко используется в кино, телевидении и веб-видео для создания эффектных сцен и анимации объектов.";
+    var icon = "photos/Ae.png";
+    var img = "photos/ae1.png";
+    var href="https://mega.nz/file/naZTDA7Z#3xU8tMk-c0hiC-d0TeWN0h9Lhy9BN3UAivTxPGXmxjI";
+    showFullScreenSquare(icon ,description, Color1, Color2, img,href);
+});
+
+document.getElementById("premiereProButton").addEventListener("click", function() {
+    const Color1 = `rgb(153, 153, 255)`;
+    const Color2 = `rgb(0, 0, 91)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Premiere Pro 2024</span><br><br>Adobe Premiere Pro — это высококачественный видеоредактор, используемый для монтажа и обработки видео. Он поддерживает множество форматов и включает профессиональные инструменты для редактирования, цветокоррекции и пост-продакшн работы.";
+    var icon = "photos/ap.png";
+    var img = "photos/ap1.png";
+    var href="https://mega.nz/file/HHIA3RZZ#WBew5hdEpTmKlSLkBId5yEQl7NHN8PuRub63RHCmuag";
+    showFullScreenSquare(icon ,description, Color1, Color2, img,href);
+});
+
+document.getElementById("animateButton").addEventListener("click", function() {
+    const Color1 = `rgb(153, 153, 255)`;
+    const Color2 = `rgb(0, 0, 91)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Animate 2024</span><br><br>Adobe Animate — это программа для создания 2D-анимированных проектов. Она идеально подходит для создания анимаций для игр, веб-сайтов и мультимедийных приложений с возможностью экспорта в различные форматы.";
+    var icon = "photos/an.png";
+    var img = "photos/an1.png";
+    var href="https://mega.nz/file/aK4EDJxJ#Ki7P-Yd6QWQIn3xCfsjwu5V699pSW-tDSRtn5lHmquc";
+    showFullScreenSquare(icon ,description, Color1, Color2, img,href);
+});
+
+document.getElementById("auditionButton").addEventListener("click", function() {
+    const Color1 = `rgb(153, 153, 255)`;
+    const Color2 = `rgb(0, 0, 91)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Audition 2024</span><br><br>Adobe Audition — это профессиональная программа для работы с аудиофайлами. Она используется для записи, редактирования и микширования звука, идеально подходя для создания подкастов, музыкальных треков и звуковых эффектов для видео.";
+    var icon = "photos/au.png";
+    var img = "photos/au1.jpg";
+    var href="https://mega.nz/file/PHJQUSSa#sLBydR43LiVqAxJhwD72O9sG4HvUw3iiKDtUpgcwhzE";
+    showFullScreenSquare(icon ,description, Color1, Color2, img,href);
+});
+
+document.getElementById("characterAnimatorButton").addEventListener("click", function() {
+    const Color1 = `rgb(153, 153, 255)`;
+    const Color2 = `rgb(0, 0, 91)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Character Animator 2024</span><br><br>Adobe Character Animator — это уникальная программа для анимации персонажей в реальном времени. Использует технологию захвата движения через веб-камеру для создания динамичных анимаций персонажей и выступлений.";
+    var icon = "photos/ch.png";
+    var img = "photos/ch1.png";
+    href="https://mega.nz/file/7SBjlLYZ#H1JnaOEEu5XfAWJ4nV-0Q_I-NnWoxi_z3y6K4LddEe8";
+    showFullScreenSquare(icon ,description, Color1, Color2, img,href);
+});
+
+document.getElementById("mediaEncodeButton").addEventListener("click", function() {
+    const Color1 = `rgb(153, 153, 255)`;
+    const Color2 = `rgb(0, 0, 91)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Media Encoder 2024</span><br><br>Adobe Media Encoder — это инструмент для кодирования и преобразования медиафайлов в различные форматы. Он широко используется для подготовки видео и аудио к экспорту в нужных разрешениях и форматах для разных платформ.";
+    var icon = "photos/me.png";
+    var img = "photos/me1.png";
+    var href="https://mega.nz/file/fHoiAYjD#7OYGYj_q9O7BgPHbSGMAT_MUBHGYIedz3kmhf9SVFDU";
+    showFullScreenSquare(icon ,description, Color1, Color2, img, href);
+});
+
+document.getElementById("dimensionButton").addEventListener("click", function() {
+    const Color1 = `rgb(51, 255, 102)`;
+    const Color2 = `rgb(1, 51, 1)`;
+    document.body.style.backgroundColor = Color1;
+    document.getElementById("leftRectangle").style.backgroundColor = Color2;
+    var description = "<span style='font-size: 26px; font-weight: bold;'>Adobe Dimension</span><br><br>Adobe Dimension — это инструмент для создания фотореалистичных 3D-композиций и визуализации проектов. Он позволяет дизайнерам легко интегрировать 3D-модели в реальные сцены, настраивать освещение, материалы и текстуры, а также быстро получать высококачественные изображения для презентаций, маркетинга и цифрового контента.";
+    var icon = "photos/dn.png";
+    var img = "photos/dn1.png";
+    var href="https://mega.nz/file/7D4E0R7B#amaz44XvUefJn9bXQmF-ma3GjGYwedi_eiT8fsnFJAE";
+    showFullScreenSquare(icon ,description, Color1, Color2, img, href);
+});
+
+function showFullScreenSquare(icon, description, c1, c2, img, href) {
+    const centerText = document.getElementById("centerText");
+    centerText.style.opacity = 0; // Скрываем центральный текст
+    
+    const fullScreenSquare = document.getElementById("fullScreenSquare");
+    fullScreenSquare.style.display = "block"; // Показать блок
+    fullScreenSquare.style.opacity = 1; // Сделать его видимым
+
+    const button = document.getElementById("instalButton");
+    const rightRectangle = document.getElementById("rightRectangle");
+    const textBlock = document.querySelector(".textInsideSquare");
+    const imageInsideSquare = document.querySelector(".imageInsideSquare");
+    const allAdobe = document.querySelector(".top-right-text");
+
+    // Убираем элементы (они исчезнут плавно)
+    rightRectangle.style.transition = "opacity 0.2s ease-out";
+    imageInsideSquare.style.transition = "opacity 0.2s ease-out";
+    button.style.transition = "opacity 0.2s ease-out";
+    textBlock.style.transition = "opacity 0.2s ease-out";
+
+    rightRectangle.style.opacity = 0;
+    imageInsideSquare.style.opacity = 0;
+    button.style.opacity = 0;
+    textBlock.style.opacity = 0;
+
+    setTimeout(() => {
+        button.style.color = c1;
+        button.style.backgroundColor = c2;
+        rightRectangle.src = img;
+        textBlock.innerHTML = description;  // Заменил textContent на innerHTML
+        imageInsideSquare.src = icon;
+        button.style.setProperty('--button-color', c1);
+        allAdobe.style.setProperty('--button-color', c1);
+        allAdobe.style.backgroundColor = c2;
+        allAdobe.style.color = c1;
+
+    
+        setTimeout(() => {
+            rightRectangle.style.opacity = 1;
+            imageInsideSquare.style.opacity = 1;
+            button.style.opacity = 1;
+            textBlock.style.opacity = 1;
+            button.onclick= function() { window.open(href, "_blank"); };
+        }, 100);
+    }, 200);
+}
